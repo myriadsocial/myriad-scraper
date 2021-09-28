3bc56386b18947c9414c917ed60a4578f16e9526
Reinaldo Gunawan <rei@kickavenue.com>
2021-09-28 11:00:31 +0800

    facebook.service/controller: added public page scraping

--- src/controllers/facebook.controller.ts
+++ src/controllers/facebook.controller.ts
@@ -1,25 +1,32 @@
 import { Controller, Get, Route, Post, Body, Path } from "tsoa";
-import { FacebookService, ImportIndividualFacebookPostParams, FbInidivdualPostsNode } from "../services/facebook.service";
+import { FacebookService } from "../services/facebook.service";
 
 @Route("facebook")
 export class FacebookController extends Controller {
-  @Post("individual")
-  public async importFacebookPostRoute(
-    @Body() body: ImportIndividualFacebookPostParams
-  ): Promise<object> {
-    return await new FacebookService().importFacebookPost(body.postId)
-  }
   @Get("individual/{postId}")
-  public async getPostById(
+  public async getPostByIdRoute(
     @Path() postId: string
   ): Promise<string> {
     const fbNode = await new FacebookService().getFacebookPostById(postId);
     return fbNode;
   }
+  @Get("page")
+  public async getFacebookNodeRoute(
+  ): Promise<object> {
+    const fbNode = await new FacebookService().getFacebookNode();
+    return fbNode;
+  }
+  @Post("individual")
+  public async importFacebookPostRoute(
+    @Body() body: { postId: string }
+  ): Promise<object> {
+    return await new FacebookService().importFacebookPost(body.postId)
+  }
+
   @Post("page")
   public async scrapeFacebookPageRoute(
-    @Body() page: string, 
-  ): Promise<boolean> {
-    return false //TODO: new FacebookService().scrapeFacebookPage(page, fetchAfterDate)
+    @Body() body: { page: string } , 
+  ): Promise<void> {
+    new FacebookService().scrapeFacebookPage(body.page)
   }
 }
--- src/services/facebook.service.ts
+++ src/services/facebook.service.ts
@@ -1,8 +1,6 @@
 import {gun} from "../app";
-
-export interface ImportIndividualFacebookPostParams {
-  postId: string;
-}
+import fs from 'fs';
+import csv from 'csv-parser';
 
 export interface FbInidivdualPostsNode {
   [key: string]: string;
@@ -13,6 +11,7 @@ interface FBPost {
   // images: string[];
   // video: string;
   // username: string;
+  post_id: string;
 }
 
 export class FacebookService {
@@ -43,5 +42,39 @@ export class FacebookService {
 
   public async getFacebookNode(): Promise<FbInidivdualPostsNode> {
     return await gun.user().get("facebook");
+  }
+
+  public async scrapeFacebookPage(page: string): Promise<void> {
+    const gunUser = gun.user();
+    const results: FBPost[] = [];
+    const {exec} = require("child_process")
+    await exec("facebook-scraper --pages 3 "+page, async (error: { message: any; }, stdout: string | string[], stderr: any) => {
+      fs.createReadStream("./"+page+"_posts.csv")
+      .pipe(csv())
+      .on('data', (data: FBPost) => results.push(data))
+      .on('end', () => {
+        results.forEach(post => {
+          console.log(post.post_id);
+          // post.text = post.text.replace(/'time'[^|]+, /g, '');
+          gunUser.get("facebook").put({[post.post_id]: post.text});
+        })
+        fs.unlink("./"+page+"_posts.csv", (err) => {
+          if (err) console.log(err);
+          console.log("file deleted successfully");
+        });  
+
+      });
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return false
+      }
+      else if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return false
+      }
+      else {
+        console.log(`stdout: ${stdout}`);
+      }
+  });
   }
 }
